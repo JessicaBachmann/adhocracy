4d4c2eff5201b4d110d65f84e5b9f15bc1c287f3
JessicaBachmann <Jessica.Bachmann@hhu.de>
2013-11-20 23:04:38 +0100

    change in table

--- src/adhocracy/migration/versions/076_login_store.py
+++ src/adhocracy/migration/versions/076_login_store.py
@@ -5,12 +5,9 @@ from sqlalchemy import Integer, DateTime, Unicode, UnicodeText
 meta = MetaData()
 
 login_table = Table('loginlog', meta,
-    Column('id', Integer, primary_key=True),
     Column('access_time', DateTime, default=datetime.utcnow),
     Column('ip_address', Unicode(255), nullable=True),
     Column('user', UnicodeText()),
-    Column('cookies', UnicodeText(), nullable=True),
-    Column('user_agent', UnicodeText(), nullable=True),
 )
 
 def upgrade(migrate_engine):
