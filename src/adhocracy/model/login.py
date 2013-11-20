65e238752cf001ae0328ac7175c2d592cfa2b5c0
JessicaBachmann <Jessica.Bachmann@hhu.de>
2013-11-20 23:05:51 +0100

    class for storing login information in table

---
+++ src/adhocracy/model/login.py
@@ -0,0 +1,50 @@
+import hashlib
+import os
+import logging
+from datetime import datetime
+
+from babel import Locale
+from pylons import config
+from pylons.i18n import _
+
+from sqlalchemy import Table, Column, func, ForeignKey, or_
+from sqlalchemy import Boolean, DateTime, Integer, Unicode, UnicodeText
+from sqlalchemy.orm import eagerload_all
+
+from adhocracy.model import meta
+from adhocracy.model import instance_filter as ifilter
+from adhocracy.model.core import JSONEncodedDict
+from adhocracy.model.core import MutationDict
+from adhocracy.model.instance import Instance
+import logging
+import meta
+from sqlalchemy import MetaData
+
+
+
+
+login_table = Table(
+    'loginlog', meta.data,
+    Column('access_time', DateTime, default=datetime.utcnow),
+    Column('ip_address', Unicode(255), nullable=True),
+    Column('user', UnicodeText()),
+)
+
+class Login(meta.Indexable):
+    
+    def __init__(self, access_time, ip_adress, user):
+        self.access_time = datetime.utcnow
+        self.ip_address = ip_adress
+        self.user = user
+    
+    @classmethod
+    def store_login_attempt(cls, ):
+        l = Login(access_time, ip_adress, user)
+        meta.Session.add(l)
+        meta.Session.flush()
+        meta.Session.commit()
+        return l
+        
+        
+    
+    
