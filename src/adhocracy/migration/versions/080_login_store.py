4f1784d422b780db7a3ed726f9003ed03a453db3
JessicaBachmann <Jessica.Bachmann@hhu.de>
2014-01-02 18:46:01 +0100

    table No changes

---
+++ src/adhocracy/migration/versions/080_login_store.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import MetaData, Table, Boolean, Column
+from sqlalchemy import Integer, DateTime, Unicode, UnicodeText
+
+meta = MetaData()
+
+login_table = Table('loginlog', meta,
+    Column('access_time', DateTime, default=datetime.utcnow),
+    Column('ip_address', Unicode(255), nullable=True),
+    Column('user', UnicodeText()),
+    Column('success', UnicodeText())
+    )
+
+
+def upgrade(migrate_engine):
+    meta.bind = migrate_engine
+    login_table.create()
